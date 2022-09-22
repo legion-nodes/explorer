f5c4c83b58fae0139ff2fd0def3fb92c00d92ab4
Legion Nodes <107527456+legion-nodes@users.noreply.github.com>
2022-09-22 11:32:03 -0400

    Rebranding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-          Ping Dashboard<small class="flow-left">Beta</small>
+          Legion Nodes Dashboard<small class="flow-left">Alpha</small>
         </h1>
       </div>
     </b-link>
